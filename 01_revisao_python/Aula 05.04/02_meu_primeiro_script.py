309a2c6dd09d9f33b4dd00f40e2d51b62529224f
Bryan-Brito <bryan.brito@academico.ifrn.edu.br>
2023-04-12 17:46:36 -0300

    Organizando as pastas por aula ministrada

---
+++ 01_revisao_python/Aula 05.04/02_meu_primeiro_script.py
@@ -0,0 +1,10 @@
+#Importando um módulo remoto
+import sys #Utilizar este módulo, para conseguir ler a versão do sistema operacional
+
+print(sys.platform) #Impressão do nome S.O.
+
+#Brincando com impressão e variáveis
+print(2**100) #Impressão da potÊncia de 2 elevado a 100
+
+x = "Spam!"
+print(x*8)
---
+++ 01_revisao_python/Aula 12.04/06_ferramentas_numericas.py
@@ -0,0 +1,27 @@
+#Ferramentas numéricas
+#Operadores de expressões
+#+, -, *, /, //, **, %, >>, etc
+
+
+#Funções matemáticas internas
+#pow, abs, round, int, hex, min, max, etc
+
+
+
+#Valor absoluto de um número
+print(abs(3.141592))
+
+#Valor arredonda de um número
+print(round(3.141592))
+print(round(3.141592, 2))
+print(round(3.141592, 3))
+
+#hex - imprime em hexa um número
+#Representação em decimal
+print(hex(255))
+#representação em binário
+print(hex(0b11111111))
+
+#Módulos utilitários
+#random, math, etc
+#>>> math.sin(2 * math.pi / 180)
