50c4a0d35a9e6759ce895d754165000a1f24be7c
Bryan-Brito <bryan.brito@academico.ifrn.edu.br>
2023-04-26 15:33:13 -0300

    Operações com tuplas

--- 01_revisao_python/Aula 24.04/18_tuplas.py
+++ 01_revisao_python/Aula 24.04/18_tuplas.py
@@ -19,3 +19,31 @@ T = tuple('spam')
 print(T)
 T = tuple(range(0,10))
 print(T)
+
+#Indexação e slide
+T = (10, 3.56, 200, 'casa')
+print(T[0])
+print(T[-1])#Último elemento
+
+print(T[1:3])#Range de elementos
+
+#Operações básicas com tuplas
+print((1,2)+(3,4))
+
+print((1,2)*3)
+
+#Iteração e associação
+T = (10, 20, 30)
+
+print(10 in T)
+
+for x in T:
+    print(x)
+
+#Tuple comprehensions
+#Recurso para criação de tuplas por iteração a outro objeto
+
+a = 'ifrn'
+res = tuple(c*2 for c in a)
+print(res)
+
