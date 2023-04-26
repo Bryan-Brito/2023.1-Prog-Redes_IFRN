ddf9cb0772332c0d2c0556f3300e8fa9d5f5e88f
Bryan-Brito <bryan.brito@academico.ifrn.edu.br>
2023-04-26 15:04:28 -0300

    Commit da aula do dia 26 de abril sobre tuplas

---
+++ 01_revisao_python/Aula 24.04/18_tuplas.py
@@ -0,0 +1,21 @@
+#Tupla vazia
+T = ()
+
+T = tuple()
+
+#Tupla com itens
+T = (4)
+print(T)
+
+T = (4,'abc',1.23,(9,8,7))
+print(T)
+
+T = 10,20
+print(T)
+print(type(T))
+
+#Tupla
+T = tuple('spam')
+print(T)
+T = tuple(range(0,10))
+print(T)
